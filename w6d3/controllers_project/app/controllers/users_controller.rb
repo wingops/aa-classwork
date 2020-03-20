bf0ef99462b43ced4346b013b16ee3577a3fd1c8
Tzvi <wingops@gmail.com>
2020-03-19 17:53:27 -0700

    rails database project

---
+++ w6d3/controllers_project/app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+    def index
+       render json: User.all
+    end
+
+    def create
+        render json: params
+    end
+    
+    def show
+        render json: params
+    end
+
+end
